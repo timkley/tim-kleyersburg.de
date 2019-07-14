b3be09dd2c558e85ff0e48129034153f0f011743
Tim Kleyersburg <tk@wacg.de>
2019-07-14 11:16:06 +0200

    new post css config

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        require('postcss-import'),
+        require('postcss-nested'),
+        require('postcss-custom-media'),
+        require('postcss-css-variables')
+    ]
+};
