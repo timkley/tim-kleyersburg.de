7132fa00e2be25e59a4c1bb9e7d334e3fcd1121d
Tim Kleyersburg <tk@wacg.de>
2024-11-03 21:02:06 +0100

    fix route name

--- routes/holocron/holocron.php
+++ routes/holocron/holocron.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::name('holocron.')->prefix('holocron')->group(function () {
-    Route::get('login', [App\Http\Controllers\Holocron\LoginController::class, 'show'])->name('login');
+    Route::get('login', [App\Http\Controllers\Holocron\LoginController::class, 'show'])->name('login-form');
     Route::post('login', [App\Http\Controllers\Holocron\LoginController::class, 'create'])->name('login');
 
     Route::group(['middleware' => 'auth'], function () {
