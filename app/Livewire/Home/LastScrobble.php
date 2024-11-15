aeb8a3c0d6fc02f1381ab404db7c0c1221134e30
Tim Kleyersburg <tk@wacg.de>
2024-11-15 07:33:41 +0100

    cache last scrobble for 5 minutes

--- app/Livewire/Home/LastScrobble.php
+++ app/Livewire/Home/LastScrobble.php
@@ -11,17 +11,19 @@ class LastScrobble extends Component
 {
     public function render()
     {
-        $response = Http::withQueryParameters([
-            'api_key' => config('services.lastfm.api_key'),
-            'format' => 'json',
-            'method' => 'user.getrecenttracks',
-            'user' => 'timmotheus',
-            'limit' => 1,
-        ])->get('http://ws.audioscrobbler.com/2.0');
+        $track = cache()->remember('last-scrobble', now()->addMinutes(5), function () {
+            $response = Http::withQueryParameters([
+                'api_key' => config('services.lastfm.api_key'),
+                'format' => 'json',
+                'method' => 'user.getrecenttracks',
+                'user' => 'timmotheus',
+                'limit' => 1,
+            ])->get('http://ws.audioscrobbler.com/2.0');
 
-        return view('livewire.home.last-scrobble', [
-            'track' => data_get($response->json(), 'recenttracks.track.0'),
-        ]);
+            return data_get($response->json(), 'recenttracks.track.0');
+        });
+
+        return view('livewire.home.last-scrobble', compact('track'));
     }
 
     public function placeholder()
