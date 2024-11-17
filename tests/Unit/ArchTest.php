af9bdb0e580858fa212d515e9dbf77931b89d500
Tim Kleyersburg <tk@wacg.de>
2024-11-17 12:48:21 +0100

    added some arch tests

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -4,5 +4,16 @@ arch('globals')
     ->expect(['dd', 'dump', 'ray'])
     ->not->toBeUsed();
 
+arch()
+    ->expect('App\Livewire')
+    ->toBeClasses()
+    ->toExtend('Livewire\Component');
+
+arch()
+    ->expect('App\Models')
+    ->toBeClasses()
+    ->toExtend('Illuminate\Database\Eloquent\Model')
+    ->ignoring('App\Models\Article');
+
 arch()->preset()->php();
 arch()->preset()->security();
