dcaef2c64b84ccbd161bc897fec319e7268e08a4
Tim Kleyersburg <tk@wacg.de>
2024-12-20 13:39:13 +0100

    better logo alignment

--- resources/views/components/layouts/holocron.blade.php
+++ resources/views/components/layouts/holocron.blade.php
@@ -2,18 +2,14 @@
     <x-slot:title>{{ $title ?? '' }}</x-slot>
     <x-slot:header>
         <div class="mx-auto max-w-5xl">
-            <div class="flex items-center justify-between gap-x-4">
-                {{-- <x-logo /> --}}
-
-                <a
-                    href="{{ route('holocron.dashboard') }}"
-                    wire:navigate
-                    class="border-0! font-mono text-sm font-semibold"
-                >
-                    <x-heroicon-o-cpu-chip class="inline-block h-6 w-6" />
-                    Holocron
-                </a>
-            </div>
+            <a
+                href="{{ route('holocron.dashboard') }}"
+                wire:navigate
+                class="!border-0 font-mono text-sm font-semibold inline-flex items-center gap-1.5"
+            >
+                <x-heroicon-o-cpu-chip class="inline-block h-6 w-6"/>
+                <span> Holocron </span>
+            </a>
         </div>
     </x-slot>
 
@@ -24,7 +20,7 @@
     <x-slot:footer></x-slot>
 
     @persist('toast')
-        <flux:toast />
+    <flux:toast/>
     @endpersist
 
     <x-slot:scripts>{{ $scripts ?? '' }}</x-slot>
