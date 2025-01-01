1c03cac34d721e308b6e6fa22f45ed34ef4db8bf
Tim Kleyersburg <tk@wacg.de>
2025-01-01 09:26:43 +0100

    feat: limit health checks to specific times

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,8 +28,8 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withSchedule(function (Illuminate\Console\Scheduling\Schedule $schedule) {
         $schedule->job(CheckForNewThings::class)->hourly()->unlessBetween('18:00', '7:00');
-        $schedule->job(CheckSufficientWaterIntake::class)->hourly();
-        $schedule->job(CheckSufficientCreatineIntake::class)->hourly();
+        $schedule->job(CheckSufficientWaterIntake::class)->hourly()->unlessBetween('20:00', '8:00');
+        $schedule->job(CheckSufficientCreatineIntake::class)->hourly()->unlessBetween('21:00', '10:00');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
