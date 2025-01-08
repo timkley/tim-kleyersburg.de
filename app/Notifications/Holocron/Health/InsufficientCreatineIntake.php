03ef24c868d261a677ce1b69f6097114a2a3ce22
Tim Kleyersburg <tk@wacg.de>
2025-01-08 21:49:30 +0100

    feat: better prompts (still not satisfied)

--- app/Notifications/Holocron/Health/InsufficientCreatineIntake.php
+++ app/Notifications/Holocron/Health/InsufficientCreatineIntake.php
@@ -26,7 +26,19 @@ class InsufficientCreatineIntake extends Notification
     public function toDiscord($notifiable)
     {
         $text = Denk::text()
-            ->prompt('Erstelle eine kurze Benachrichtigung um Tim daran zu erinnern, sein Kreatin einzunehmen. Keine Emojis.')
+            ->temperature(1.1)
+            ->systemPrompt(<<<EOT
+Your job is to create notifications.
+
+- answer in german
+- make sure german grammar and dictation is correct, don't answer before you are sure it is correct
+- direct the message to the user
+- be consice
+- no emojis
+- be funny
+EOT
+)
+            ->prompt('Tim hat heute noch kein Kreatin eingenommen.')
             ->generate();
 
         return DiscordMessage::create($text);
--- app/Notifications/Holocron/Health/InsufficientWaterIntake.php
+++ app/Notifications/Holocron/Health/InsufficientWaterIntake.php
@@ -28,7 +28,19 @@ class InsufficientWaterIntake extends Notification
         $remainingInLiters = round($this->remaining / 1000, 1);
 
         $text = Denk::text()
-            ->prompt('Erstelle eine kurze Benachrichtigung um Tim daran zu erinnern, genug Wasser zu trinken. Er muss heute noch '.$remainingInLiters.' Liter trinken. Keine Emojis.')
+            ->temperature(1.1)
+            ->systemPrompt(<<<EOT
+Your job is to create notifications.
+
+- answer in german
+- make sure german grammar and dictation is correct, don't answer before you are sure it is correct
+- direct the message to the user
+- be consice
+- no emojis
+- be funny
+EOT
+            )
+            ->prompt('Tim muss heute noch '.$remainingInLiters.' Liter Wasser trinken.')
             ->generate();
 
         return DiscordMessage::create($text);
