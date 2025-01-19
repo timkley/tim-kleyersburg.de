0495362ef70a1eac2e31305a08fa59cc8151f6a2
Tim Kleyersburg <tk@wacg.de>
2025-01-19 20:31:43 +0100

    feat: adjust goal prompt

--- app/Notifications/Holocron/Health/GoalsNotReached.php
+++ app/Notifications/Holocron/Health/GoalsNotReached.php
@@ -27,22 +27,26 @@ class GoalsNotReached extends Notification
     public function toDiscord($notifiable)
     {
         $missedGoals = $this->missedGoals->map(function ($goal) {
-            return "- {$goal->type->value} - achieved: {$goal->amount} - goal: {$goal->goal}";
+            return "- {$goal->type->value} - achieved: {$goal->amount} - goal: {$goal->goal} {$goal->type->unit}";
         })->implode(PHP_EOL);
 
+        $date = now()->toDateString();
+        $time = now()->toTimeString();
         $text = Denk::text()
             ->systemPrompt(
-                <<<'EOT'
+                <<<EOT
 Your job is to create notifications.
+Today is the $date, it is currently $time, adjust the message accordingly.
 
 - answer in german
 - make sure german grammar and dictation is correct, don't answer before you are sure it is correct
-- direct the message to the user
-- be concise
+- direct the message at the user
+- be concise and keep it as short as possible
+- be motivational
 - no emojis
 - you can be humorous
 
-Users name: Tim
+User name: Tim
 EOT
             )
             ->prompt("Create a notification for the not achieved goals: $missedGoals")
