a1c32506f7ee6d9d7f98c7c8c424ffaba9ad4c44
Shift <shift@laravelshift.com>
2025-03-06 07:23:42 +0000

    Convert string references to `::class`
    PHP 5.5.9 adds the new static `class` property which provides the fully qualified class name. This is preferred over using strings for class names since the `class` property references are checked by PHP.

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -15,7 +15,7 @@ arch()
     ->expect('App\Models')
     ->toBeClasses()
     ->toExtend('Illuminate\Database\Eloquent\Model')
-    ->ignoring('App\Models\Article');
+    ->ignoring(\App\Models\Article::class);
 
 arch()->preset()->php();
 arch()->preset()->security();
