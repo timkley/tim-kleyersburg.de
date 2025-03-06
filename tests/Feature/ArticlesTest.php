591f6f1efdd7a0828cc16e34fbdd40f9b340979a
Shift <shift@laravelshift.com>
2025-03-06 07:23:41 +0000

    Apply code style

--- tests/Feature/ArticlesTest.php
+++ tests/Feature/ArticlesTest.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 use function Pest\Laravel\get;
 
 it('shows the article overview', function () {
-get('/articles')
-->assertSuccessful();
-    });
+    get('/articles')
+        ->assertSuccessful();
+});
 
 it('shows an article', function () {
-get('/articles/resize-images-with-mogrify')
-->assertSuccessful()
-->assertSee('Use ImageMagicks mogrify CLI to batch resize images');
-    });
+    get('/articles/resize-images-with-mogrify')
+        ->assertSuccessful()
+        ->assertSee('Use ImageMagicks mogrify CLI to batch resize images');
+});
