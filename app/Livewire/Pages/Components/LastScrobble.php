aa978ebb3b6e9e7353128f8e7f2225f546018803
Tim Kleyersburg <tk@wacg.de>
2025-04-22 11:36:59 +0200

    feat: add top artist

--- app/Livewire/Pages/Components/LastScrobble.php
+++ app/Livewire/Pages/Components/LastScrobble.php
@@ -14,19 +14,32 @@ class LastScrobble extends Component
 {
     public function render(): View
     {
-        $track = cache()->remember('last-scrobble', now()->addMinutes(5), function () {
+        $track = cache()->remember('lastfm:last-scrobble', now()->addMinutes(5), function () {
             $response = Http::withQueryParameters([
                 'api_key' => config('services.lastfm.api_key'),
                 'format' => 'json',
                 'method' => 'user.getrecenttracks',
                 'user' => 'timmotheus',
                 'limit' => 1,
-            ])->get('http://ws.audioscrobbler.com/2.0');
+            ])->get('https://ws.audioscrobbler.com/2.0');
 
             return data_get($response->json(), 'recenttracks.track.0');
         });
 
-        return view('pages.components.last-scrobble', compact('track'));
+        $topArtist = cache()->remember('lastfm:weekly-artist', now()->addWeek(), function () {
+            $response = Http::withQueryParameters([
+                'api_key' => config('services.lastfm.api_key'),
+                'format' => 'json',
+                'method' => 'user.gettopartists',
+                'user' => 'timmotheus',
+                'limit' => 1,
+                'period' => '3month',
+            ])->get('https://ws.audioscrobbler.com/2.0');
+
+            return data_get($response->json(), 'topartists.artist.0');
+        });
+
+        return view('pages.components.last-scrobble', compact('track', 'topArtist'));
     }
 
     public function placeholder()
--- resources/views/pages/components/last-scrobble.blade.php
+++ resources/views/pages/components/last-scrobble.blade.php
@@ -1,4 +1,4 @@
-<div class="mx-auto h-48 w-64 rounded bg-sky-100 p-2 shadow-md transition hover:bg-sky-100/80 sm:w-60 dark:bg-slate-700 dark:hover:bg-slate-700/80">
+<div class="mx-auto h-auto w-64 rounded bg-sky-100 p-2 shadow-md transition hover:bg-sky-100/80 sm:w-60 dark:bg-slate-700 dark:hover:bg-slate-700/80">
     <div class="mb-5 text-center">
         <span class="text-sm text-gray-700 dark:text-gray-300"> Last Scrobble </span>
         <a href="https://www.last.fm/user/Timmotheus">@timmotheus</a>
@@ -15,11 +15,23 @@
             />
             <div class="truncate font-bold">{{ $track['name'] }}</div>
             <div class="truncate text-sm">{{ $track['artist']['#text'] }}</div>
+
+            <hr class="opacity-25 my-4 mx-8">
+
+            <div class="mb-2 truncate text-xs opacity-50">Top artist last 3 months</div>
+            <div class="font-bold">
+                {{ $topArtist['name'] }} <span class="font-normal text-sm">{{ Number::format($topArtist['playcount']) }} plays</span>
+            </div>
         @else
             <div class="animate-pulse text-center">
                 <div class="mx-auto mb-2 h-16 w-16 rounded bg-sky-50 dark:bg-slate-800"></div>
-                <div class="mb-2 h-4 rounded-full bg-sky-50 dark:bg-slate-800"></div>
+                <div class="mb-2 h-5 rounded-full bg-sky-50 dark:bg-slate-800"></div>
                 <div class="h-4 rounded-full bg-sky-50 dark:bg-slate-800"></div>
+
+                <hr class="opacity-25 my-4">
+
+                <div class="mb-2 h-4 rounded-full bg-sky-50 dark:bg-slate-800"></div>
+                <div class="h-6 rounded-full bg-sky-50 dark:bg-slate-800"></div>
             </div>
         @endif
     </div>
