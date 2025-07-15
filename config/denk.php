60759f72b95b63e6629606ff80032eb44bee1836
Tim Kleyersburg <tk@wacg.de>
2025-07-15 15:33:05 +0200

    chore: update config

--- config/denk.php
+++ config/denk.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'openai_api_key' => env('OPENAI_API_KEY'),
+    'openai_api_key' => env('OPENROUTER_API_KEY'),
     'base_uri' => 'openrouter.ai/api/v1',
 ];
