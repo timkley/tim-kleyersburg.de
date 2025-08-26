b0a4f2172178bb9dd80a7f3f37ef077c51948309
Tim Kleyersburg <tk@wacg.de>
2025-08-26 18:24:46 +0200

    fix: link display

--- modules/Holocron/Quest/Views/components/link.blade.php
+++ modules/Holocron/Quest/Views/components/link.blade.php
@@ -1,26 +1,30 @@
-<flux:text class="flex gap-x-2 items-center" x-data="{ editing: false}">
-    <flux:link
-        class="break-all flex-1"
-        href="{{ $url }}"
-        target="_blank"
-        x-show="!editing"
-    >
-        {{ $title }}
-    </flux:link>
-    <flux:field
-        class="flex-1"
-        x-show="editing"
-    >
-        <flux:input
-            x-ref="editTitle"
-            wire:model.live.debounce="title"
-            x-on:keyup.enter="editing = false"
-            x-on:keyup.escape="editing = false"
-        />
-    </flux:field>
+<div class="flex gap-x-2 items-center" x-data="{ editing: false}">
+    <div class="flex-1">
+        <flux:text>
+            <flux:link
+                class="break-all flex-1"
+                href="{{ $url }}"
+                target="_blank"
+                x-show="!editing"
+            >
+                {{ $title }}
+            </flux:link>
+        </flux:text>
+        <flux:field
+            class="flex-1"
+            x-show="editing"
+        >
+            <flux:input
+                x-ref="editTitle"
+                wire:model.live.debounce="title"
+                x-on:keyup.enter="editing = false"
+                x-on:keyup.escape="editing = false"
+            />
+        </flux:field>
+    </div>
     <flux:button
         variant="filled"
         icon="pencil"
         x-on:click="editing = !editing; $nextTick(() => $refs.editTitle.focus())"
     />
-</flux:text>
+</div>
