d45a9077c674018c9656fffe103d40a8619ced09
Tim Kleyersburg <tk@wacg.de>
2025-09-13 21:32:28 +0200

    feat: load API routes in QuestServiceProvider

--- modules/Holocron/Quest/QuestServiceProvider.php
+++ modules/Holocron/Quest/QuestServiceProvider.php
@@ -14,6 +14,7 @@ class QuestServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
 
         // Load routes
+        $this->loadRoutesFrom(__DIR__.'/Routes/api.php');
         $this->loadRoutesFrom(__DIR__.'/Routes/web.php');
 
         // Load views and assign namespace 'order'
