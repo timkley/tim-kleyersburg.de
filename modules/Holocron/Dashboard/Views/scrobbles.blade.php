eb14401a75502e18f67f8627b87aa74947ced43c
Tim Kleyersburg <tk@wacg.de>
2025-09-26 16:37:29 +0200

    feat: implement pagination for scrobbles and enhance display formatting

--- modules/Holocron/Dashboard/Views/scrobbles.blade.php
+++ modules/Holocron/Dashboard/Views/scrobbles.blade.php
@@ -1,11 +1,22 @@
-<div>
-    <div class="font-medium mb-4">{{ $count }} Scrobbles</div>
+@use(Illuminate\Support\Number)
 
-    @foreach($scrobbles as $scrobble)
-        <p>
-            {{ $scrobble->artist }} -
-            {{ $scrobble->track }}
-            {{ $scrobble->played_at }}
-        </p>
-    @endforeach
+<div class="space-y-4">
+    <flux:heading size="xl">
+        {{ Number::format(number: $count, locale: 'de-DE') }} Scrobbles
+    </flux:heading>
+
+    <div class="space-y-1">
+        @foreach($scrobbles as $scrobble)
+            <div class="flex justify-between">
+                <div>
+                    {{ $scrobble->artist }} - {{ $scrobble->track }}
+                </div>
+                <div class="font-mono">
+                    {{ $scrobble->played_at->shiftTimezone('UTC')->setTimezone(config('app.timezone'))->format('d.m.Y H:i') }}
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <flux:pagination :paginator="$scrobbles" />
 </div>
--- modules/Holocron/_Shared/Livewire/Scrobbles.php
+++ modules/Holocron/_Shared/Livewire/Scrobbles.php
@@ -7,14 +7,17 @@ namespace Modules\Holocron\_Shared\Livewire;
 use App\Models\Scrobble;
 use Illuminate\View\View;
 use Livewire\Attributes\Title;
+use Livewire\WithPagination;
 
 #[Title('Scrobbles')]
 class Scrobbles extends HolocronComponent
 {
+    use WithPagination;
+
     public function render(): View
     {
         return view('holocron-dashboard::scrobbles', [
-            'scrobbles' => Scrobble::query()->limit(100)->latest('played_at')->get(),
+            'scrobbles' => Scrobble::query()->latest('played_at')->paginate(100),
             'count' => Scrobble::query()->count(),
         ]);
     }
