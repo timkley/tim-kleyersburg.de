a1f5512b6ea70d754d223b6764516e07ab5653be
Tim Kleyersburg <tk@wacg.de>
2025-10-09 09:05:50 +0200

    fix: make image base64 encoded

--- modules/Holocron/Dashboard/Views/print-weather.blade.php
+++ modules/Holocron/Dashboard/Views/print-weather.blade.php
@@ -9,12 +9,20 @@
 @endsection
 
 @section('content')
+    @php
+        $iconPath = public_path("img/weather_icons/{$forecast->wmoCode}d_big.png");
+        $iconBase64 = file_exists($iconPath) ? base64_encode(file_get_contents($iconPath)) : null;
+    @endphp
     <div class="text-center">
         <p>
             {{ $forecast->date->translatedFormat('D d.m.') }}
         </p>
         <p class="text-lg">
-            <img class="wmo" src="/img/weather_icons/{{ $forecast->wmoCode }}d_big.png" alt="{{ $forecast->condition }}">
+            @if($iconBase64)
+                <img class="wmo" src="data:image/png;base64,{{ $iconBase64 }}" alt="{{ $forecast->condition }}">
+            @else
+                <span>{{ $forecast->condition }}</span>
+            @endif
         </p>
         <p>
             {{ $forecast->minTemp }}° / {{ $forecast->maxTemp }}°
