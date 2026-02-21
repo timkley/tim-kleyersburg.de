e90d821cb19fc98cf4e47469466b24bcfaa7a55d
Tim Kleyersburg <tk@wacg.de>
2026-02-21 21:01:24 +0100

    feat(grind): add nutrition_daily_targets column to user_settings

--- modules/Holocron/User/Models/UserSetting.php
+++ modules/Holocron/User/Models/UserSetting.php
@@ -12,6 +12,7 @@ use Modules\Holocron\User\Database\Factories\UserSettingFactory;
 /**
  * @property-read float $weight
  * @property-read bool $printer_silenced
+ * @property-read ?array $nutrition_daily_targets
  */
 class UserSetting extends Model
 {
@@ -29,5 +30,15 @@ class UserSetting extends Model
     protected static function newFactory(): UserSettingFactory
     {
         return UserSettingFactory::new();
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'nutrition_daily_targets' => 'array',
+        ];
     }
 }
